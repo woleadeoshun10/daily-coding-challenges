be6d168b4230a78d27da10add11f3858ec31c800
Adewole Adeoshun <146618781+woleadeoshun10@users.noreply.github.com>
2025-07-18 00:47:51 -0400

    Update div_by_4_or_6_not_both.cpp

--- div_by_4_or_6_not_both.cpp
+++ div_by_4_or_6_not_both.cpp
@@ -1,1 +1,27 @@
+// Print numbers from 1 to 100 divisible by 4 or 6 but not both
+// Print 5 numbers per line
+// Show count and sum
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0, sum = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        if ((i % 4 == 0 || i % 6 == 0) && i % 12 != 0) {
+            cout << i << " ";
+            count++;
+            sum += i;
+
+            // print 5 numbers per line
+            if (count % 5 == 0) cout << endl;
+        }
+    }
+
+    cout << "\nCount: " << count << endl;
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
 
