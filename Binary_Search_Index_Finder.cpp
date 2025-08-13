095f9918bf883d187d80b74923e6c55377a3e0a4
Adewole Adeoshun <146618781+woleadeoshun10@users.noreply.github.com>
2025-08-12 23:01:52 -0400

    Update Binary_Search_Index_Finder.cpp

--- Binary_Search_Index_Finder.cpp
+++ Binary_Search_Index_Finder.cpp
@@ -8,9 +8,7 @@ Your implementation should be efficient and should follow the binary search algo
 */
 
 
-include <iostream>
-#include <vector>
-using namespace std;
+
 
 // Function to perform binary search
 int binarySearch(vector<int>& nums, int target){
